a8690b71a6e65984ae47e594b71522a052781566
abidhossain <abid161999@gmail.com>
2023-05-24 21:30:53 +0600

    finished REST API

--- server/routes/todoListRoutes.js
+++ server/routes/todoListRoutes.js
@@ -13,4 +13,33 @@ router.post("/api/list", async (req, res) => {
   }
 });
 
+router.get("/api/lists", async (req, res) => {
+  try {
+    const allTodoList = await todoListModel.find({});
+    res.status(200).json(allTodoList);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.put("/api/list/:id", async (req, res) => {
+  try {
+    const updateList = await todoListModel.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json("Task Updated");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.delete("/api/list/:id", async (req, res) => {
+  try {
+    const DeleteList = await todoListModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Task Deleted");
+  } catch (err) {
+    res.json(err);
+  }
+});
+
 module.exports = router;
